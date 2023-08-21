c43620737bd34140426c440e60ad4f9aa84ec82e
rohit-gangshettiwar-bbi <142482494+rohit-gangshettiwar-bbi@users.noreply.github.com>
2023-08-21 17:19:14 +0530

    Add files via upload

--- Assignment/js/script.js
+++ Assignment/js/script.js
@@ -1,1 +1,11 @@
 
+const userPasswordEl = document.querySelector("#password");
+const togglePasswordEl = document.querySelector("#togglePassword");
+
+togglePasswordEl.addEventListener("click", function () {
+  if (this.checked === true) {
+    userPasswordEl.setAttribute("type", "text");
+  } else {
+    userPasswordEl.setAttribute("type", "password");
+  }
+});
